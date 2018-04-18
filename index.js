dc97d4369cd64bfe78939b259162cffef6bba1f9
Javier Jimenez <javierrcc522@gmail.com>
2018-04-17 20:57:19 -0700

    made a component that returns some amount of jsx react native and render it on the screen

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//import a library to help create a component
 import React from 'react';
 import ReactNative from 'react-native';
+
+// Create a component
+const App = () => {
+  return (
+    <Text>Some text</Text>
+  );
+};
