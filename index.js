3106032079972938798243019ffc98d2a4e12f50
Javier Jimenez <javierrcc522@gmail.com>
2018-04-24 13:32:59 -0700

    made album list component

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
 const App = () => (
-  <Header headerWords={'Albums'} />
+  <View>
+    <Header headerWords={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 AppRegistry.registerComponent('albums', () => App);
