41ed722b0c3054298040dbce69a12c75d6a24b02
Javier Jimenez <javierrcc522@gmail.com>
2018-05-20 23:20:01 -0700

    added album artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,13 +4,16 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
+
   const {
     thumbnailStyle,
     headerContentStyle,
     thumbnailContainerStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
+  
   return (
     <Card>
       <CardSection>
@@ -24,6 +27,12 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }} />
+
       </CardSection>
     </Card>
   );
@@ -46,6 +55,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
