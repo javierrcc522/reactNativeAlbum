2efd08ca8e76af9053aedd9ed6be46346819603d
Javier Jimenez <javierrcc522@gmail.com>
2018-05-22 12:56:46 -0700

    text style

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,6 +11,14 @@ const Button = () => {
 };
 
 const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
   buttonStyle: {
     flex: 1,
     alignSelf: 'stretch',
