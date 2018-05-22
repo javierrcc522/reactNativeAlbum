f0f9964329cc673b03fc3dce1d5b5cf1f39e44bc
Javier Jimenez <javierrcc522@gmail.com>
2018-05-22 12:48:16 -0700

    add styles to the click button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const { bottonStyle } = styles;
   return (
-    <TouchableOpacity>
+    <TouchableOpacity style={bottonStyle}>
       <Text>Click me</Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
